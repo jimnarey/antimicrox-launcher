f942abd6b6d25adeb83a23a3bd987795279074bb
Jim Narey <jimnarey@me.com>
2023-12-30 23:41:15 +0000

    Read netlink message headers

--- netlink.py
+++ netlink.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python3
 
+# import sys
 import socket
 import struct
 
-NETLINK_PROTOCOL = 11
-PROCESS_INDEX = 1
-# PROCESS_VALUE = 1
+NETLINK_CONNECTOR = 11
+CN_IDX_PROC = 1
+CN_VAL_PROC = 1
 
-NLMSG_ERROR = 0x2
 
-NLMSG_HDRLEN = struct.calcsize("=LHHLL")
+def parse_msg_header(nlmsg):
+    hdr = {}
+    hdr["len"], hdr["type"], hdr["flags"], hdr["seq"], hdr["pid"] = struct.unpack(
+        "=LHHLL", nlmsg[:16]
+    )
+    return hdr
 
 
 def create_nl_socket():
-    sock = socket.socket(socket.AF_NETLINK, socket.SOCK_RAW, NETLINK_PROTOCOL)
-    sock.bind((0, PROCESS_INDEX))
+    sock = socket.socket(socket.AF_NETLINK, socket.SOCK_RAW, NETLINK_CONNECTOR)
+    sock.bind((0, CN_IDX_PROC))
     return sock
 
 
-def recv_nl_message(sock):
+def recv_nl_messages(sock):
     while True:
-        data = sock.recv(65535)
-        nlmsghdr = data[:NLMSG_HDRLEN]
-        msg_type, _, _, _, _ = struct.unpack("=LHHLL", nlmsghdr)
-        if msg_type == NLMSG_ERROR:
-            break
-        yield data
+        try:
+            data = sock.recv(65535)
+            # data = sock.recv(131070)
+            msg_header = parse_msg_header(data)
+            print(msg_header)
+        except socket.error as e:
+            print("Socket error:", e)
 
 
 def monitor_process_events():
     nl_socket = create_nl_socket()
-    try:
-        for msg in recv_nl_message(nl_socket):
-            print("Received:", msg)
-    finally:
-        nl_socket.close()
+    if nl_socket:
+        try:
+            recv_nl_messages(nl_socket)
+        finally:
+            nl_socket.close()
+    else:
+        print("Failed to create socket")
 
 
 if __name__ == "__main__":
